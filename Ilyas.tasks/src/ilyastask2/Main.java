2df3bc32bb99638eb821767ea0b788f692cc5298
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-13 19:24:06 -0800

    Begin 2

--- Ilyas.tasks/src/ilyastask2/Main.java
+++ Ilyas.tasks/src/ilyastask2/Main.java
@@ -3,6 +3,7 @@ package ilyastask2;
 public class Main {
     public static void main(String[] args){
         int g = 5;
-        System.out.println("S=5*5=25" );
+        long r = 25;
+        System.out.println("S=" + r);
     }
 }
