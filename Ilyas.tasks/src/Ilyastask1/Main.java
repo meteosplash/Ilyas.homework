c4c0539ae7e2f1b673bddf9d392f3b8fb9e937f4
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-13 19:35:07 -0800

    Begin 1

--- Ilyas.tasks/src/Ilyastask1/Main.java
+++ Ilyas.tasks/src/Ilyastask1/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         int a = 4;
-        System.out.println("P=" + 4*a );
+        int p = 16;
+        System.out.println("P=" + p);
 
     }
 }
