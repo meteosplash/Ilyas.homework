0afdc4cc36a90bd19a6d017fc9efea14400c5494
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-13 19:45:23 -0800

    Begin 1

---
+++ Ilyas.tasks/src/ilyastask3/Main.java
@@ -0,0 +1,12 @@
+package ilyastask3;
+
+public class Main {
+    public static void main(String[] args) {
+        short a = 4;
+        short b = 6;
+        short s = 24;
+        short p = 20;
+        System.out.println("Square=" + s);
+        System.out.println("Perimeter=" + p);
+    }
+}
