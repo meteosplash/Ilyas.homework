28241cafc61378ce94632b960ffebe2365bb6212
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-14 19:24:36 -0800

    Begin 4

---
+++ Ilyas.tasks/src/ilyastask4/Main.java
@@ -0,0 +1,11 @@
+package ilyastask4;
+
+public class Main {
+    public static void main(String[] args) {
+        float d = 7.8f;
+        float pi = 3.14f;
+        float l = d*pi;
+        System.out.println("Длина окружности(L)=" + l);
+
+    }
+}
