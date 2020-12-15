d313286010114e52dd5a893a719766b2b9fc28c7
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-14 19:33:38 -0800

    Begin 5

---
+++ Ilyas.tasks/src/ilyastask5/Main.java
@@ -0,0 +1,13 @@
+package ilyastask5;
+
+public class Main {
+    public static void main(String[] args) {
+    byte a = 4;
+    byte v = 4*4*4;
+    int s = 6*a*a;
+        System.out.println("Площадь поверхности="+ s);
+        System.out.println("Объём куба="+ v);
+
+
+    }
+}
