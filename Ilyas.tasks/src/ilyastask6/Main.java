f4f8d0604e6526232ebed2c12532bf31c5e402fa
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-14 19:49:51 -0800

    Begin 6

---
+++ Ilyas.tasks/src/ilyastask6/Main.java
@@ -0,0 +1,13 @@
+package ilyastask6;
+
+public class Main {
+    public static void main(String[] args) {
+        short a = 3;
+        short b = 4;
+        short c = 5;
+        short v = 60;
+        short s = 2*(3*4+4*5+3*5);
+        System.out.println("Oбъём="+ v);
+        System.out.println("Площадь="+ s);
+    }
+}
