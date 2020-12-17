94a634c456fc5f03e869888ef3a68fdd8471d11b
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 19:36:58 -0800

    Begin 10

---
+++ Ilyas.tasks/src/ilyastask10/Main.java
@@ -0,0 +1,18 @@
+package ilyastask10;
+
+public class Main {
+    public static void main(String[] args) {
+        short s= 9;
+        short i = 25;
+        short u = 34;
+        short h = 16;
+        short e= 225;
+        float t = 2.7f;
+
+                System.out.println("Сумма квадратов 3 и 5 = "+ u);
+        System.out.println("Разность квадратов 3 и 5 = "+ h);
+        System.out.println("Произведение квадратов 3 и 5 = "+ e);
+        System.out.println("Частное квадратов 3 и 5 = "+ t);
+    }
+}
+
