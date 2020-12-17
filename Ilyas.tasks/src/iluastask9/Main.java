aa5d0a2927b4914b759f94280cd65b533979aa6f
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 19:26:14 -0800

    Begin 9

---
+++ Ilyas.tasks/src/iluastask9/Main.java
@@ -0,0 +1,10 @@
+package iluastask9;
+
+public class Main {
+    public static void main(String[] args) {
+        short d = 5;
+        short i = 7;
+        float y =15.65f;
+        System.out.println("Cреднее геометрическое 5 и 7 = "+ y);
+    }
+}
