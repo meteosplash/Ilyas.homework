250df13658af58bab25b887d370411033bfb432b
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 19:54:50 -0800

    Begin 14

---
+++ Ilyas.tasks/src/ilyastask14/Main.java
@@ -0,0 +1,11 @@
+package ilyastask14;
+
+public class Main {
+    public static void main(String[] args) {
+        float g = 50.24f;
+        short i = 8;
+        float r = 200.96f;
+        System.out.println("Радиус = "+ i);
+        System.out.println("Площадь = "+ r);
+    }
+}
