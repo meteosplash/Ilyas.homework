25e2d0be6b83622da9dc7066e4ccc90c290dcb0f
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 18:43:50 -0800

    Begin 6

---
+++ Ilyas.tasks/src/ilyastask7/Main.java
@@ -0,0 +1,12 @@
+package ilyastask7;
+
+public class Main {
+    public static void main(String[] args) {
+        short r = 13;
+        float l = 2*r*3.14f;
+        float s = r*r*3.14f;
+        System.out.println("Длина окружности="+ l);
+        System.out.println("Площадь окружности="+ s);
+
+    }
+}
