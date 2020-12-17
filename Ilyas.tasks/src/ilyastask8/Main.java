ad339a4ae38ea7a3b2e7232a5df02b3ff7c630de
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 18:58:49 -0800

    Begin 8

---
+++ Ilyas.tasks/src/ilyastask8/Main.java
@@ -0,0 +1,10 @@
+package ilyastask8;
+
+public class Main {
+    public static void main(String[] args) {
+        short a = 10;
+        short u = 18;
+        short f = 9;
+        System.out.println("Среднее арифметическое чисел 10 и 18= "+ f);
+    }
+}
