1aee253ebd8aa8ac21942a60be7bff8a2b80e09b
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 19:43:45 -0800

    Begin 10

---
+++ Ilyas.tasks/src/llyastask11/MAin.java
@@ -0,0 +1,16 @@
+package llyastask11;
+
+public class MAin {
+    public static void main(String[] args) {
+        short r = 14;
+        short k = 11;
+        byte l = 25;
+        byte c = 3;
+        short j = 154;
+        float e = 1.27f;
+        System.out.println("Сумма = "+ l);
+        System.out.println("Разность = "+ c);
+        System.out.println("Произведение = "+ 154);
+        System.out.println("Частное ="+ e);
+    }
+}
