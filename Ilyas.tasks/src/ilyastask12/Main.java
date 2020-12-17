3bba3200eee22936cb63a782ee69c33f410ac205
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-16 19:48:19 -0800

    Begin 11

---
+++ Ilyas.tasks/src/ilyastask12/Main.java
@@ -0,0 +1,13 @@
+package ilyastask12;
+
+public class Main {
+    public static void main(String[] args) {
+        short s = 3;
+        short h =4;
+        short j =5;
+        short u = 12;
+        short g = 5;
+        System.out.println("P="+ u);
+        System.out.println("C= "+ j);
+    }
+}
