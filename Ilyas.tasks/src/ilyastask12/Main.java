26627c1ffd503bb22d206e068408de45b7f3b784
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-25 19:23:40 -0800

    Begin 12

--- Ilyas.tasks/src/ilyastask12/Main.java
+++ Ilyas.tasks/src/ilyastask12/Main.java
@@ -2,12 +2,12 @@ package ilyastask12;
 
 public class Main {
     public static void main(String[] args) {
-        short s = 3;
-        short h =4;
-        short j = 5;
+        int s = 3;
+        int h =4;
+        int j = 5;
         int p = s+h+j;
-        double u = Math.sqrt(Math.pow(3,2)+Math.pow(4,2));
-        System.out.println("P="+ p);
+        double u = Math.sqrt(Math.pow(s,2)+Math.pow(h,2));
+        System.out.println("P= "+ p);
         System.out.println("C= "+ u);
 
     }
