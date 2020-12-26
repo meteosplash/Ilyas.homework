1717e67f81fd2e22cd5c93cb2d76f81b91946ceb
sulikprogram <mamutovsulejman2@gmail.com>
2020-12-25 19:31:13 -0800

    Begin 13

--- Ilyas.tasks/src/ilyastask13/Main.java
+++ Ilyas.tasks/src/ilyastask13/Main.java
@@ -2,11 +2,12 @@ package ilyastask13;
 
 public class Main {
     public static void main(String[] args) {
-        short r1= 9;
-        short r2= 7;
-        double s1= Math.pow(9,2)*3.14;
-        double s2= Math.pow(7,2)*3.14;
-        double s3= (Math.pow(9,2)*3.14) - (Math.pow(7,2)*3.14);
+        short r1 = 9;
+        short r2 = 7;
+        double pi = 3.14;
+        double s1= (Math.pow(r1,2))* pi;
+        double s2= (Math.pow(r2,2))* pi;
+        double s3= (Math.pow(r1,2))* pi - (Math.pow(r2,2))* pi;
         System.out.println("S1= "+s1);
         System.out.println("S2= "+s2);
         System.out.println("S3= "+s3);
