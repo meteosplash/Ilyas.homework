87e0944971248e41006c6ade9e1c6043955608d2
sulikprogram <mamutovsulejman2@gmail.com>
2021-01-03 10:16:35 -0800

    Begin 14

--- Ilyas.tasks/src/ilyastask14/Main.java
+++ Ilyas.tasks/src/ilyastask14/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         double l = 50.24d;
         double pi = 3.14d;
-        double r = l/ (2*pi);
-        double s = (Math.pow(r,2))* pi;
+        int t = 2;
+        double r = (l/ (t*pi));
+        double s = ((Math.pow(r,t))* pi);
         System.out.println("Радиус = "+ r);
         System.out.println("Площадь = "+ s);
     }
