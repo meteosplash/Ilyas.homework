69eb75d09257ee38e44fdcb36fe36723c273fdf5
sulikprogram <mamutovsulejman2@gmail.com>
2021-01-04 18:43:36 -0800

    Begin 23

---
+++ Ilyas.tasks/src/ilyastask23/Main.java
@@ -0,0 +1,19 @@
+package ilyastask23;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите 2 целых числа");
+        Scanner top = new Scanner(System.in);
+        int x = top.nextInt();
+        Scanner top2 = new Scanner(System.in);
+        int y = top2.nextInt();
+        if(x>y){
+            System.out.println(x+ " большее число, меньшее число "+ y);
+        }
+        if(y>x){
+            System.out.println(y + " большее число, меньшее число " + x);
+        }
+    }
+}
