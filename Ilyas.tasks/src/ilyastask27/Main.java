1d2496e9222a1a467c7f95592a2fcd9779af4ea8
sulikprogram <mamutovsulejman2@gmail.com>
2021-01-09 18:29:23 -0800

    Begin 27

---
+++ Ilyas.tasks/src/ilyastask27/Main.java
@@ -0,0 +1,24 @@
+package ilyastask27;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner task = new Scanner(System.in);
+        double a = task.nextDouble();
+        double b = task.nextDouble();
+        double c = task.nextDouble();
+        if (a < b & a < c) {
+            System.out.println("1-e число меньшее");
+        }
+        else if (b < a & b < c) {
+            System.out.println("2-e число меньшее");
+        }
+        else if (c < b & c < a) {
+            System.out.println("3-e число меньшее");
+        }
+        else{
+                System.out.println("Наименьшего числа нет");
+            }
+        }
+    }
