db7ef0de33e2a6618aedd4814db78105f6ad1ec8
sulikprogram <mamutovsulejman2@gmail.com>
2021-01-18 17:05:40 -0800

    Begin 32

---
+++ Ilyas.tasks/src/ilyastask32/Main.java
@@ -0,0 +1,19 @@
+package ilyastask32;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 7;
+        a--;
+        int n = b-a;
+        a++;
+        int i;
+        for(i=3;i<7;i=i+0) {
+            if(i>0){
+                i++;
+                System.out.println(" "+ i );
+        }
+        }
+        System.out.println("Количество чисел "+n);
+    }
+}
