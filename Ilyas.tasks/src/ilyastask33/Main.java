a1992a7840509d8ce5c9cec2f9539fde78965dc1
sulikprogram <mamutovsulejman2@gmail.com>
2021-01-18 17:06:18 -0800

    Begin 33

---
+++ Ilyas.tasks/src/ilyastask33/Main.java
@@ -0,0 +1,12 @@
+package ilyastask33;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 2;
+
+        for(a=7;a<b;a--)
+                System.out.println(""+ a );
+        }
+    }
+
