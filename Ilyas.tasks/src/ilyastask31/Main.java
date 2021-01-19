c0d2626e7df7d57854f570988f32b05233772fc3
sulikprogram <mamutovsulejman2@gmail.com>
2021-01-18 17:03:27 -0800

    Begin 31

---
+++ Ilyas.tasks/src/ilyastask31/Main.java
@@ -0,0 +1,12 @@
+package ilyastask31;
+
+public class Main {
+    public static void main(String[] args) {
+        int  k = 6;
+        int n = 7;
+        for(n=7;n<14;n=n+1){
+                System.out.println(" " + k);
+            }
+        }
+    }
+
