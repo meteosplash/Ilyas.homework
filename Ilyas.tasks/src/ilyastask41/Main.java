d797e6527972d1cd05f49cc0ef4c13caaa79080b
123 <magomedovabdurahman518@gmail.com>
2021-01-20 09:56:28 +0300

    ilyastask 41

---
+++ Ilyas.tasks/src/ilyastask41/Main.java
@@ -0,0 +1,16 @@
+package ilyastask41;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner en = new Scanner(System.in);
+        System.out.print("N = ");
+        int n = en.nextInt();
+        int all = 0;
+        for(int a = 0;a<=n;a++){
+            all+=Math.pow(n+a,2);
+        }
+        System.out.println("Сумма квадратов чисел от N до 2*N = "+all);
+    }
+}
