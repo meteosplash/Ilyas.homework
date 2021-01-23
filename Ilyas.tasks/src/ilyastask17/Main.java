6513edb258ef6d58f7206e25dcf573cb4e9d2de6
sulikprogram <mamutovsulejman2@gmail.com>
2021-01-22 23:43:22 -0800

    task17

--- Ilyas.tasks/src/ilyastask17/Main.java
+++ Ilyas.tasks/src/ilyastask17/Main.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bведите число ");
+        System.out.println("Bведите целое число ");
         Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
+        int a = sc.nextInt();
         if(a>0){
             a++;
         }
         else {
             a=a-2;
         }
-        System.out.println("Вы ввели число "+a);
+        System.out.println("В результате операций введённое число = "+a);
+        System.out.println("Условия операций читать в задачнике");
     }
 }
