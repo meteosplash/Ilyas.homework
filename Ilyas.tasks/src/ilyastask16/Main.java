8a0b5d390d1be758c7db2b5af99617e980b42706
sulikprogram <mamutovsulejman2@gmail.com>
2021-01-22 23:42:54 -0800

    task16

--- Ilyas.tasks/src/ilyastask16/Main.java
+++ Ilyas.tasks/src/ilyastask16/Main.java
@@ -6,10 +6,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Введите положительное число ");
         Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
+        int a = sc.nextInt();
         if (a > 0) {
             a++;
+            System.out.println(a + " - это ваше число увеличенное на 1");
         }
-        System.out.println(a + " - это ваше число увеличенное на 1");
+        if(a<=0){
+            System.out.println("Прочитайте условие еще раз ");
+            System.out.println("и запустите программу заново");
+
+        }
     }
 }
