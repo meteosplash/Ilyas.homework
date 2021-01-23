ae8fa9f74455212b6a956de8798a03de26e891ee
sulikprogram <mamutovsulejman2@gmail.com>
2021-01-22 23:11:42 -0800

    task45

---
+++ Ilyas.tasks/src/ilyastask45/Main.java
@@ -0,0 +1,16 @@
+package ilyastask45;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner lastfor = new Scanner(System.in);
+        int a = lastfor.nextInt();
+        int b = lastfor.nextInt();
+        int sum = a;
+        for (int i = b;i<=b;i++){
+            sum=sum*a;
+        }
+        System.out.println(""+sum);
+    }
+}
