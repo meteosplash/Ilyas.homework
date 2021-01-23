ab671c258b77296b26a9178c9a507f4a6841211c
sulikprogram <mamutovsulejman2@gmail.com>
2021-01-23 02:21:54 -0800

    task19

---
+++ Ilyas.tasks/src/ilyastask19/Main.java
@@ -0,0 +1,27 @@
+package ilyastask19;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите 3 целых числа через enter");
+        Scanner if4 = new Scanner(System.in);
+        int a = if4.nextInt();
+        int b = if4.nextInt();
+        int c = if4.nextInt();
+        int n = 0;
+        //c n будет происходить операция инкремента
+        //в том операторе где значение - true
+        //т.е число положительное
+        if(a>0){
+            n++;
+        }
+        if(b>0){
+            n++;
+        }
+        if(c>0){
+            n++;
+        }
+        System.out.println("Количество положительных чисел из представленных выше - "+n);
+    }
+}
