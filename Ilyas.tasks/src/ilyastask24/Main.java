a97585fdc4bb2f3e3ff58a429471dd6ceb5ef222
sulikprogram <mamutovsulejman2@gmail.com>
2021-01-27 10:42:47 -0800

    task 24

---
+++ Ilyas.tasks/src/ilyastask24/Main.java
@@ -0,0 +1,23 @@
+package ilyastask24;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner if24 = new Scanner(System.in);
+        System.out.println("Введите 2 целых числа");
+        int a = if24.nextInt();
+        int b = if24.nextInt();
+        int sum = a+b;
+        if(a>b) {
+            a = sum-a;
+            b = sum - b;
+        }
+            if(a<b){
+                a=sum-a;
+                b=sum-b;
+            }
+        System.out.println("Теперь 1-е число = "+a+", а 2-е = "+b);
+        }
+    }
+
