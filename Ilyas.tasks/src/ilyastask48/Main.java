53d0892f80bb47758e9c52408ae88d2da31771d5
sulikprogram <mamutovsulejman2@gmail.com>
2021-02-05 06:51:06 -0800

    task 48

--- Ilyas.tasks/src/ilyastask48/Main.java
+++ Ilyas.tasks/src/ilyastask48/Main.java
@@ -1,14 +1,18 @@
 package ilyastask48;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int n = 7;
-        int k = 2;
+        System.out.println("Enter whole positive numbers");
+        Scanner while3 = new Scanner(System.in);
+        int n = while3.nextInt();
+        int k = while3.nextInt();
         int ch = 0;
         while(n>=k) {
             ch++;
             n-=k;
         }
-        System.out.println("chastnoe= "+ch+" a ostatok= "+n);
+        System.out.println("Chastnoe= "+ch+", a ostatok= "+n+".");
     }
 }
