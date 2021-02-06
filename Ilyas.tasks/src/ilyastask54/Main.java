20b5578330ca9549a1bb4a588a455600d504d02b
sulikprogram <mamutovsulejman2@gmail.com>
2021-02-06 07:48:45 -0800

    task 54

---
+++ Ilyas.tasks/src/ilyastask54/Main.java
@@ -0,0 +1,16 @@
+package ilyastask54;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter number N");
+        Scanner while9 = new Scanner(System.in);
+        int n= while9.nextInt();
+        double k = 1;
+        {
+
+        }
+
+    }
+}
