6f8f27822a8180af0747d509f65e991c6487c90c
sulikprogram <mamutovsulejman2@gmail.com>
2021-02-07 09:15:06 -0800

    task 54

--- Ilyas.tasks/src/ilyastask54/Main.java
+++ Ilyas.tasks/src/ilyastask54/Main.java
@@ -7,10 +7,10 @@ public class Main {
         System.out.println("Enter number N");
         Scanner while9 = new Scanner(System.in);
         int n= while9.nextInt();
-        double k = 1;
-        {
-
+        int k = 1;
+        while(3*k<=n){
+            k++;
         }
-
+        System.out.println("k= "+k);
     }
 }
