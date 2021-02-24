b5a4208e88c58f837680f6000c27ff231ae7d05b
sulikprogram <mamutovsulejman2@gmail.com>
2021-02-24 06:26:09 -0800

    task 51

--- Ilyas.tasks/src/ilyastask52/Main.java
+++ Ilyas.tasks/src/ilyastask52/Main.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter number n");
+        System.out.println("Enter number N");
         Scanner while7 = new Scanner(System.in);
         int n = while7.nextInt();
+        int schet = 0;
         int k = 1;
-        while (k*k>n){
+        while (schet<=n){
             k++;
+            schet = k*k;
         }
         System.out.println(""+k);
     }
